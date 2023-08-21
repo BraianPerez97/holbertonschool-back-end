2d73d9fba283647dee7b0548e5407e2d2d50310b
BraianPerez97 <3603@holbertonstudents.com>
2023-08-21 15:55:54 -0400

    documented script

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+"""Script for use a REST API for employee ID
+returns there TODO list progress"""
 import requests
 import sys
 
 if __name__ == '__main__':
 
-    # get employee ID from command line argument
+    """get employee ID from command line argument"""
     if len(sys.argv) < 2:
         print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
         sys.exit(1)
     employee_id = int(sys.argv[1])
 
-    # Make API request
+    """Make API request"""
     response = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(
             employee_id)
     )
     data = response.json()
 
-    # Parse data
+    """Parse data"""
     employee_name = data[0]["userId"]
     total_task = len(data)
     done_task = [task for task in data if task["completed"]]
 
-    # Print report
+    """Print report"""
     print("Employee {} is done with task ({}/{}):".format(
         employee_name, len(done_task), total_task)
     )
